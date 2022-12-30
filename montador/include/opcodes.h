b27d2fc4e7c3cfe208e259fd2edd767cc5a7418a
murilo-simio <murilo2110@hotmail.com>
2022-12-30 13:03:27 -0300

    fix: merged changes

--- montador/include/opcodes.h
+++ montador/include/opcodes.h
@@ -1,9 +1,6 @@
-#include <map>
-#include <string>
-
 #define ADD 1
 #define SUB 2
-#define MULT 3
+#define MUL 3
 #define DIV 4
 #define JMP 5
 #define JMPN 6
@@ -20,21 +17,3 @@
 #define DOIS_PTS ':'
 #define PONTO_VIR ';'
 #define VIRGULA ','
-
-// std::map<std::string, int> operacoes = 
-// {
-//     {"ADD",    1},   
-//     {"SUB",    2},
-//     {"MUL",    3},
-//     {"DIV",    4},
-//     {"JMP",    5},
-//     {"JMPN",   6},
-//     {"JMPP",   7},
-//     {"JMPZ",   8},
-//     {"COPY",   9},
-//     {"LOAD",   10},
-//     {"STORE",  11},
-//     {"INPUT",  12},
-//     {"OUTPUT", 13},
-//     {"STOP",   14}
-// };
